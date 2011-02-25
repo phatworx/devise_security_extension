032c06a82081fba6e22d6b61fba22adade099f12
Christoph Chilian <dev@cc-web.eu>
2011-02-25 11:24:07 +0100

    Fix: Save old password in archive only on update

--- lib/devise_security_extension/models/password_archivable.rb
+++ lib/devise_security_extension/models/password_archivable.rb
@@ -10,7 +10,7 @@ module Devise # :nodoc:
         base.class_eval do
           include InstanceMethods
           has_many :old_passwords, :as => :password_archivable, :class_name => "OldPassword"
-          before_save :archive_password
+          before_update :archive_password
           validate :validate_password_archive
         end
       end
