bb5f1b585d5cfd4ec94002224f0702796eb3720d
Szymon Fiedler <fidelski@gmail.com>
2011-05-09 12:52:42 +0200

    added fix for after_authentication

--- lib/devise_security_extension/hooks/password_expirable.rb
+++ lib/devise_security_extension/hooks/password_expirable.rb
@@ -1,3 +1,5 @@
 Warden::Manager.after_authentication do |record, warden, options|
-  warden.session(options[:scope])[:password_expired] = record.need_change_password?
+  if record.respond_to?(:need_change_password?)
+    warden.session(options[:scope])[:password_expired] = record.need_change_password?
+  end
 end
