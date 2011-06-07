2afed0ad0869da0ee8e6f8fb346ed872692aef57
Marco Scholl <develop@marco-scholl.de>
2011-06-07 12:02:13 +0200

    use new configurations

--- lib/devise_security_extension/patches.rb
+++ lib/devise_security_extension/patches.rb
@@ -4,10 +4,10 @@ module DeviseSecurityExtension
     
     class << self
       def apply
-        Devise::PasswordsController.send(:include, Patches::ControllerCaptcha) if Devise.recover_captcha
-        Devise::UnlocksController.send(:include, Patches::ControllerCaptcha) if Devise.unlock_captcha
-        Devise::RegistrationsController.send(:include, Patches::ControllerCaptcha) if Devise.sign_up_captcha
-        Devise::SessionsController.send(:include, Patches::ControllerCaptcha) if Devise.sign_in_captcha
+        Devise::PasswordsController.send(:include, Patches::ControllerCaptcha) if Devise.captcha_for_recover
+        Devise::UnlocksController.send(:include, Patches::ControllerCaptcha) if Devise.captcha_form_unlock
+        Devise::RegistrationsController.send(:include, Patches::ControllerCaptcha) if Devise.captcha_for_sign_up
+        Devise::SessionsController.send(:include, Patches::ControllerCaptcha) if Devise.captcha_form_sign_in
       end
     end
   end
