059b246412f5243a8b3a90fd8b2cbedb50973b3a
Marco Scholl <develop@marco-scholl.de>
2012-02-06 13:47:33 +0100

    mongoid sample for old passwords

--- lib/devise_security_extension/models/password_expirable.rb
+++ lib/devise_security_extension/models/password_expirable.rb
@@ -17,46 +17,46 @@ module Devise # :nodoc:
 
 
 
-        # is an password change required?
-        def need_change_password?
-          if self.class.expire_password_after.is_a? Fixnum or self.class.expire_password_after.is_a? Float
-            self.password_changed_at.nil? or self.password_changed_at < self.class.expire_password_after.ago
-          else
-            false
-          end
-        end
-
-        # set a fake datetime so a password change is needed and save the record
-        def need_change_password!
-          if self.class.expire_password_after.is_a? Fixnum or self.class.expire_password_after.is_a? Float
-            need_change_password
-            self.save(:validate => false)
-          end
-        end
-
-        # set a fake datetime so a password change is needed
-        def need_change_password
-          if self.class.expire_password_after.is_a? Fixnum or self.class.expire_password_after.is_a? Float
-            self.password_changed_at = self.class.expire_password_after.ago
-          end
-
-          # is date not set it will set default to need set new password next login
-          need_change_password if self.password_changed_at.nil?
-
-          self.password_changed_at
-        end
-
-        private
-
-        # is password changed then update password_cahanged_at
-        def update_password_changed
-          self.password_changed_at = Time.now if (self.new_record? or self.encrypted_password_changed?) and not self.password_changed_at_changed?
+      # is an password change required?
+      def need_change_password?
+        if self.class.expire_password_after.is_a? Fixnum or self.class.expire_password_after.is_a? Float
+          self.password_changed_at.nil? or self.password_changed_at < self.class.expire_password_after.ago
+        else
+          false
         end
       end
 
-      module ClassMethods #:nodoc:
-        ::Devise::Models.config(self, :expire_password_after)
+      # set a fake datetime so a password change is needed and save the record
+      def need_change_password!
+        if self.class.expire_password_after.is_a? Fixnum or self.class.expire_password_after.is_a? Float
+          need_change_password
+          self.save(:validate => false)
+        end
       end
+
+      # set a fake datetime so a password change is needed
+      def need_change_password
+        if self.class.expire_password_after.is_a? Fixnum or self.class.expire_password_after.is_a? Float
+          self.password_changed_at = self.class.expire_password_after.ago
+        end
+
+        # is date not set it will set default to need set new password next login
+        need_change_password if self.password_changed_at.nil?
+
+        self.password_changed_at
+      end
+
+      private
+
+      # is password changed then update password_cahanged_at
+      def update_password_changed
+        self.password_changed_at = Time.now if (self.new_record? or self.encrypted_password_changed?) and not self.password_changed_at_changed?
+      end
+    end
+
+    module ClassMethods #:nodoc:
+      ::Devise::Models.config(self, :expire_password_after)
+    end
 
   end
 
