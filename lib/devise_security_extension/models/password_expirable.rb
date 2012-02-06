a2b31267f1dd4ad62ad0da4ee0750db544c848e8
Christoph Chilian <dev@cc-web.eu>
2012-02-06 11:50:18 +0100

    closed #22

--- lib/devise_security_extension/models/password_expirable.rb
+++ lib/devise_security_extension/models/password_expirable.rb
@@ -19,7 +19,7 @@ module Devise # :nodoc:
 
         # is an password change required?
         def need_change_password?
-          if self.class.expire_password_after.is_a? Fixnum
+          if self.class.expire_password_after.is_a? Fixnum or self.class.expire_password_after.is_a? Float
             self.password_changed_at.nil? or self.password_changed_at < self.class.expire_password_after.ago
           else
             false
@@ -28,7 +28,7 @@ module Devise # :nodoc:
 
         # set a fake datetime so a password change is needed and save the record
         def need_change_password!
-          if self.class.expire_password_after.is_a? Fixnum
+          if self.class.expire_password_after.is_a? Fixnum or self.class.expire_password_after.is_a? Float
             need_change_password
             self.save(:validate => false)
           end
@@ -36,7 +36,7 @@ module Devise # :nodoc:
 
         # set a fake datetime so a password change is needed
         def need_change_password
-          if self.class.expire_password_after.is_a? Fixnum
+          if self.class.expire_password_after.is_a? Fixnum or self.class.expire_password_after.is_a? Float
             self.password_changed_at = self.class.expire_password_after.ago
           end
 
