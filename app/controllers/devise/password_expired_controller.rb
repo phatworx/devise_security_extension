1cbef9f0fc881ad83f5fc7b5dbc6199b0d097a51
Marco Scholl <develop@marco-scholl.de>
2012-11-21 14:33:57 +0100

    Fix: Now use respond_with for using scoped views

--- app/controllers/devise/password_expired_controller.rb
+++ app/controllers/devise/password_expired_controller.rb
@@ -1,11 +1,10 @@
 class Devise::PasswordExpiredController < DeviseController
   skip_before_filter :handle_password_change
   prepend_before_filter :authenticate_scope!, :only => [:show, :update]
-  include Devise::Controllers::Helpers
 
   def show
     if not resource.nil? and resource.need_change_password?
-      render 'devise/password_expired/show'
+      respond_with(resource)
     else
       redirect_to :root
     end
@@ -19,7 +18,7 @@ class Devise::PasswordExpiredController < DeviseController
       redirect_to stored_location_for(scope) || :root
     else
       clean_up_passwords(resource)
-      render 'devise/password_expired/show'
+      respond_with(resource, action: :show)
     end
   end
 
