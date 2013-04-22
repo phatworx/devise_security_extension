7c528c926590d54ce817b3c0d9e9ae261bf429e5
Alexander Dreher <alexdreher@lxmedia.net>
2013-04-22 10:59:03 +0200

    security_questionable doc

--- lib/devise_security_extension/models/security_questionable.rb
+++ lib/devise_security_extension/models/security_questionable.rb
@@ -3,10 +3,13 @@ module Devise
     # SecurityQuestionable is an accessible add-on for visually handicapped people,
     # to ship around the captcha with screenreader compatibility.
     #
-    # You need to add two text_field_tags to the associated form:
+    # You need to add two text_field_tags to the associated forms (unlock,
+    # password, confirmation):
     # :security_question_answer and :captcha
     #
     # And add the security_question to the register/edit form.
+    # f.select :security_question_id, SecurityQuestion.where(locale: I18n.locale).map{|s| [s.name, s.id]}
+    # f.text_field :security_question_answer
     module SecurityQuestionable
       extend ActiveSupport::Concern
 
