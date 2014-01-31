346c1be9b23f4959ad6fafb0c3db26696f79674d
Alexander Dreher <alexdreher@lxmedia.net>
2014-01-31 16:07:40 +0100

    fixes registrations_controller_captcha (empty params)

--- lib/devise_security_extension/patches/registrations_controller_captcha.rb
+++ lib/devise_security_extension/patches/registrations_controller_captcha.rb
@@ -3,25 +3,26 @@ module DeviseSecurityExtension::Patches
     extend ActiveSupport::Concern
     included do
       define_method :create do
-        build_resource
+        build_resource(sign_up_params)
 
         if valid_captcha? params[:captcha]
 
           if resource.save
+            yield resource if block_given?
             if resource.active_for_authentication?
-              set_flash_message :notice, :signed_up if is_navigational_format?
-              sign_in(resource_name, resource)
+              set_flash_message :notice, :signed_up if is_flashing_format?
+              sign_up(resource_name, resource)
               respond_with resource, :location => after_sign_up_path_for(resource)
             else
-              set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
-              expire_session_data_after_sign_in!
+              set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+              expire_data_after_sign_in!
               respond_with resource, :location => after_inactive_sign_up_path_for(resource)
             end
           else
             clean_up_passwords resource
             respond_with resource
           end
-
+          
         else
           resource.errors.add :base, t('devise.invalid_captcha')
           clean_up_passwords resource
