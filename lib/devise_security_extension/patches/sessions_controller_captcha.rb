316b683e8f3253524743d3772f75cac5df30e56f
Alexander Dreher <alexdreher@lxmedia.net>
2014-01-31 16:17:44 +0100

    fixes sessions_controller_captcha (params)

--- lib/devise_security_extension/patches/sessions_controller_captcha.rb
+++ lib/devise_security_extension/patches/sessions_controller_captcha.rb
@@ -4,12 +4,13 @@ module DeviseSecurityExtension::Patches
     included do
       define_method :create do
         if valid_captcha? params[:captcha]
-          resource = warden.authenticate!(auth_options)
-          set_flash_message(:notice, :signed_in) if is_navigational_format?
+          self.resource = warden.authenticate!(auth_options)
+          set_flash_message(:notice, :signed_in) if is_flashing_format?
           sign_in(resource_name, resource)
+          yield resource if block_given?
           respond_with resource, :location => after_sign_in_path_for(resource)
         else
-          flash[:alert] = t('devise.invalid_captcha') if is_navigational_format?
+          flash[:alert] = t('devise.invalid_captcha') if is_flashing_format?
           respond_with({}, :location => new_session_path(resource_name))
         end
       end
