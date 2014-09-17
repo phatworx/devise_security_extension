2613d1d1b8a726085ca0dbb8c81cf8b65315a869
Nick Karpenske <nick.karpenske@digitaloffersengineering.com>
2014-09-17 04:16:34 -0500

    [#78037390] Fix broken password_expirable extension

--- lib/devise_security_extension/controllers/helpers.rb
+++ lib/devise_security_extension/controllers/helpers.rb
@@ -6,12 +6,12 @@ module DeviseSecurityExtension
       included do
         before_filter :handle_password_change
       end
-      
+
       module ClassMethods
         # helper for captcha
         def init_recover_password_captcha
           include RecoverPasswordCaptcha
-        end        
+        end
       end
 
       module RecoverPasswordCaptcha
@@ -22,40 +22,41 @@ module DeviseSecurityExtension
 
       # controller instance methods
 
-        private
+      private
 
-        # lookup if an password change needed
-        def handle_password_change
-          if not devise_controller? and not ignore_password_expire? and not request.format.nil? and request.format.html?
-            Devise.mappings.keys.flatten.any? do |scope|
-              if signed_in?(scope) and warden.session(scope)['password_expired']
-                session["#{scope}_return_to"] = request.path if request.get?
-                redirect_for_password_change scope
-                return
-              end
+      # lookup if an password change needed
+      def handle_password_change
+        unless ignore_password_expire?
+          Devise.mappings.keys.flatten.any? do |scope|
+            if signed_in?(scope) and warden.session(scope)['password_expired']
+              session["#{scope}_return_to"] = request.path if request.get?
+              redirect_for_password_change scope
+              return
             end
           end
         end
+      end
 
-        # redirect for password update with alert message
-        def redirect_for_password_change(scope)
-          redirect_to change_password_required_path_for(scope), :alert => I18n.t('change_required', {:scope => 'devise.password_expired'})
-        end
+      # redirect for password update with alert message
+      def redirect_for_password_change(scope)
+        redirect_to change_password_required_path_for(scope), :alert => I18n.t('change_required', {:scope => 'devise.password_expired'})
+      end
 
-        # path for change password
-        def change_password_required_path_for(resource_or_scope = nil)
-          scope       = Devise::Mapping.find_scope!(resource_or_scope)
-          change_path = "#{scope}_password_expired_path"
-          send(change_path)
-        end
-        
-        protected
-        
-        # allow to overwrite for some special handlings
-        def ignore_password_expire?
-          false
-        end
+      # path for change password
+      def change_password_required_path_for(resource_or_scope = nil)
+        scope       = Devise::Mapping.find_scope!(resource_or_scope)
+        change_path = "#{scope}_password_expired_path"
+        send(change_path)
+      end
 
+      protected
+
+      def ignore_password_expire?
+        devise_controller? ||
+          request.format.nil? ||
+          !request.format.html? ||
+          warden.nil?
+      end
 
     end
   end
