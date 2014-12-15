08cf801e74f445ac30adacff10279ca978d1125a
Ricardo Brazão <ricardo.p.brazao@gmail.com>
2014-12-15 20:30:13 +0000

    clear raw_session

--- lib/devise_security_extension/hooks/session_limitable.rb
+++ lib/devise_security_extension/hooks/session_limitable.rb
@@ -19,6 +19,7 @@ Warden::Manager.after_set_user :only => :fetch do |record, warden, options|
 
   if record.respond_to?(:unique_session_id) && warden.authenticated?(scope) && options[:store] != false
     if record.unique_session_id != warden.session(scope)['unique_session_id'] && !env['devise.skip_session_limitable']
+      warden.raw_session.clear
       warden.logout(scope)
       throw :warden, :scope => scope, :message => :session_limited
     end
