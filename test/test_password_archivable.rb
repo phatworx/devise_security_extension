bc2793c077749e89ec3353d01f788c9023bdb838
Thomas Powell <twilliampowell@gmail.com>
2015-03-28 12:14:12 -0400

    Trying to exercise more of the code.

--- test/test_password_archivable.rb
+++ test/test_password_archivable.rb
@@ -1,6 +1,6 @@
 require 'helper'
 
-class TestPasswordArchivable < ActiveSupport::TestCase
+class TestPasswordArchivableOn < ActiveSupport::TestCase
   setup do
     Devise.password_archiving_count = 2
   end
@@ -10,10 +10,11 @@ class TestPasswordArchivable < ActiveSupport::TestCase
   end
 
   test "should respect maximum attempts configuration" do
+    User.deny_old_passwords = true
     user = User.new
     user.password = 'password1'
     user.password_confirmation = 'password1'
-    user.save!
+    assert_nothing_raised { user.save! }
 
     user.password = 'password1'
     user.password_confirmation = 'password1'
@@ -21,27 +22,67 @@ class TestPasswordArchivable < ActiveSupport::TestCase
   end
 
   test 'the option should be dynamic during runtime' do
+    User.deny_old_passwords = true
     class ::User
       def archive_count
         1
       end
+      def confirmed_password= password
+        self.password = password
+        self.password_confirmation = password
+        self.save!
+      end
     end
 
     user = User.new
-    user.password = 'password1'
-    user.password_confirmation = 'password1'
-    user.save!
+    assert_nothing_raised { user.confirmed_password = 'password1' }
+    assert_nothing_raised  { user.confirmed_password = 'password2' }
 
-    user.password = 'password2'
-    user.password_confirmation = 'password2'
-    user.save!
+    assert_raises(ActiveRecord::RecordInvalid) { user.confirmed_password = 'password2' }
 
-    user.password = 'password2'
-    user.password_confirmation = 'password2'
-    assert_raises(ActiveRecord::RecordInvalid) { user.save! }
-
-    user.password = 'password1'
-    user.password_confirmation = 'password1'
-    assert_raises(ActiveRecord::RecordInvalid) { user.save! }
+    assert_raises(ActiveRecord::RecordInvalid) { user.confirmed_password = 'password1' }
   end
 end
+class TestPasswordArchivableOff < ActiveSupport::TestCase
+  setup do
+    Devise.password_archiving_count = 2
+  end
+
+  teardown do
+    Devise.password_archiving_count = 1
+  end
+
+  test "should respect maximum attempts configuration" do
+    User.deny_old_passwords = false
+    user = User.new
+    user.password = 'password1'
+    user.password_confirmation = 'password1'
+    assert_nothing_raised { user.save! }
+
+    user.password = 'password1'
+    user.password_confirmation = 'password1'
+    assert_nothing_raised { user.save! }
+  end
+
+  test 'the option should be dynamic during runtime' do
+    class ::User
+      def archive_count
+        1
+      end
+      def confirmed_password= password
+        self.password = password
+        self.password_confirmation = password
+        self.save!
+      end
+    end
+
+    user = User.new
+    assert_nothing_raised { user.confirmed_password = 'password1' }
+    assert_nothing_raised  { user.confirmed_password = 'password2' }
+
+    assert_nothing_raised { user.confirmed_password = 'password2' }
+
+    assert_nothing_raised { user.confirmed_password = 'password1' }
+  end
+end
+
