021e214f59d7d9bf1aaaed2baa4f60f79b1d44dd
Mario Manno <mma@i22.de>
2016-03-10 11:02:24 +0100

    clean up password archivable

--- lib/devise_security_extension/models/password_archivable.rb
+++ lib/devise_security_extension/models/password_archivable.rb
@@ -1,37 +1,36 @@
 module Devise
   module Models
-
     # PasswordArchivable
     module PasswordArchivable
-      extend  ActiveSupport::Concern
+      extend ActiveSupport::Concern
 
       included do
-        has_many :old_passwords, :as => :password_archivable, :dependent => :destroy
+        has_many :old_passwords, as: :password_archivable, dependent: :destroy
         before_update :archive_password
         validate :validate_password_archive
       end
 
       def validate_password_archive
-        self.errors.add(:password, :taken_in_past) if encrypted_password_changed? and password_archive_included?
+        errors.add(:password, :taken_in_past) if encrypted_password_changed? and password_archive_included?
       end
 
       # validate is the password used in the past
       def password_archive_included?
-        unless self.deny_old_passwords.is_a? Fixnum
-          if self.deny_old_passwords.is_a? TrueClass and archive_count > 0
+        unless deny_old_passwords.is_a? Fixnum
+          if deny_old_passwords.is_a? TrueClass and archive_count > 0
             self.deny_old_passwords = archive_count
           else
             self.deny_old_passwords = 0
           end
         end
 
-        if self.deny_old_passwords > 0 and not self.password.nil?
-          old_passwords_including_cur_change = self.old_passwords.order(:id).reverse_order.limit(self.deny_old_passwords)
-          old_passwords_including_cur_change << OldPassword.new(old_password_params)  # include most recent change in list, but don't save it yet!
+        if deny_old_passwords > 0 and !password.nil?
+          old_passwords_including_cur_change = old_passwords.order(:id).reverse_order.limit(deny_old_passwords)
+          old_passwords_including_cur_change << OldPassword.new(old_password_params) # include most recent change in list, but don't save it yet!
           old_passwords_including_cur_change.each do |old_password|
             dummy                    = self.class.new
             dummy.encrypted_password = old_password.encrypted_password
-            return true if dummy.valid_password?(self.password)
+            return true if dummy.valid_password?(password)
           end
         end
 
@@ -59,18 +58,18 @@ module Devise
 
       # archive the last password before save and delete all to old passwords from archive
       def archive_password
-        if self.encrypted_password_changed?
+        if encrypted_password_changed?
           if archive_count.to_i > 0
-            self.old_passwords.create! old_password_params
-            self.old_passwords.order(:id).reverse_order.offset(archive_count).destroy_all
+            old_passwords.create! old_password_params
+            old_passwords.order(:id).reverse_order.offset(archive_count).destroy_all
           else
-            self.old_passwords.destroy_all
+            old_passwords.destroy_all
           end
         end
       end
 
       def old_password_params
-        { encrypted_password: self.encrypted_password_change.first }
+        { encrypted_password: encrypted_password_change.first }
       end
 
       module ClassMethods
@@ -78,5 +77,4 @@ module Devise
       end
     end
   end
-
 end
