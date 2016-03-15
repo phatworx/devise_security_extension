7525e665d9abc81643d5f8afc73f35479a3cb8a5
Mike Rogers <me@mikerogers.io>
2016-03-15 22:54:25 +0100

    Moving captcha and security_question into before_action
    It follows the example of
    https://github.com/plataformatec/devise/wiki/How-To:-Use-Recaptcha-with-Devise
    
    I'm not 100% the best way to test this functionality, but I've added
    some thoughts :)

--- lib/devise_security_extension/patches.rb
+++ lib/devise_security_extension/patches.rb
@@ -11,16 +11,16 @@ module DeviseSecurityExtension
 
     class << self
       def apply
-        Devise::PasswordsController.send(:include, Patches::PasswordsControllerCaptcha) if Devise.captcha_for_recover or Devise.security_question_for_recover
-        Devise::UnlocksController.send(:include, Patches::UnlocksControllerCaptcha) if Devise.captcha_for_unlock or Devise.security_question_for_unlock
-        Devise::ConfirmationsController.send(:include, Patches::ConfirmationsControllerCaptcha) if Devise.captcha_for_confirmation
+        Devise::PasswordsController.send(:include, Patches::ControllerCaptcha) if Devise.captcha_for_recover or Devise.security_question_for_recover
+        Devise::UnlocksController.send(:include, Patches::ControllerCaptcha) if Devise.captcha_for_unlock or Devise.security_question_for_unlock
+        Devise::ConfirmationsController.send(:include, Patches::ControllerCaptcha) if Devise.captcha_for_confirmation
 
-        Devise::PasswordsController.send(:include, Patches::PasswordsControllerSecurityQuestion) if Devise.security_question_for_recover
-        Devise::UnlocksController.send(:include, Patches::UnlocksControllerSecurityQuestion) if Devise.security_question_for_unlock
-        Devise::ConfirmationsController.send(:include, Patches::ConfirmationsControllerSecurityQuestion) if Devise.security_question_for_confirmation
+        Devise::PasswordsController.send(:include, Patches::ControllerSecurityQuestion) if Devise.security_question_for_recover
+        Devise::UnlocksController.send(:include, Patches::ControllerSecurityQuestion) if Devise.security_question_for_unlock
+        Devise::ConfirmationsController.send(:include, Patches::ControllerSecurityQuestion) if Devise.security_question_for_confirmation
 
-        Devise::RegistrationsController.send(:include, Patches::RegistrationsControllerCaptcha) if Devise.captcha_for_sign_up
-        Devise::SessionsController.send(:include, Patches::SessionsControllerCaptcha) if Devise.captcha_for_sign_in
+        Devise::RegistrationsController.send(:include, Patches::ControllerCaptcha) if Devise.captcha_for_sign_up
+        Devise::SessionsController.send(:include, Patches::ControllerCaptcha) if Devise.captcha_for_sign_in
       end
     end
   end
