81b1f74617b4487d8be0e6c5423e7f9ff1f8bf2e
Nate Bird <natebird@gmail.com>
2016-06-08 15:52:05 -0400

    Update the syntax for a couple environment configuration changes.

--- test/dummy/config/environments/test.rb
+++ test/dummy/config/environments/test.rb
@@ -2,8 +2,8 @@ RailsApp::Application.configure do
   config.cache_classes = true
   config.eager_load = false
 
-  config.serve_static_files = true
-  config.static_cache_control = 'public, max-age=3600'
+  config.public_file_server.enabled = true
+  config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=3600' }
 
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
