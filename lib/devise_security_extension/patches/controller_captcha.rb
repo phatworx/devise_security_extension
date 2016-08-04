70d9bf7da6fc6941d3a056aee75d8bef8e889108
Matthew Ingle <matthew.ingle@zuora.com>
2016-08-04 19:11:32 -0400

    Resource was null if recaptcha was not compeleted or invalid this made displaying error messages difficult

--- lib/devise_security_extension/patches/controller_captcha.rb
+++ lib/devise_security_extension/patches/controller_captcha.rb
@@ -3,12 +3,12 @@ module DeviseSecurityExtension::Patches
     extend ActiveSupport::Concern
 
     included do
-      prepend_before_action :check_captcha, only: [:create]
+      append_before_action :check_captcha, only: [:create]
     end
 
     private
     def check_captcha
-      return if ((defined? verify_recaptcha) && (verify_recaptcha)) || ((defined? valid_captcha?) && (valid_captcha? params[:captcha]))
+      return resource if ((defined? verify_recaptcha) && (verify_recaptcha)) || ((defined? valid_captcha?) && (valid_captcha? params[:captcha]))
 
       flash[:alert] = t('devise.invalid_captcha') if is_navigational_format?
       respond_with({}, location: url_for(action: :new))
