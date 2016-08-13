855ac1e380019a656d5747f4daf2602aa012ef76
Hidekazu Tanaka <hidekazu.tanaka@gmail.com>
2016-08-13 11:07:13 +0900

    Fix Rails 5 deprecation warning for to_prepare

--- lib/devise_security_extension/rails.rb
+++ lib/devise_security_extension/rails.rb
@@ -3,10 +3,15 @@ module DeviseSecurityExtension
     ActiveSupport.on_load(:action_controller) do
       include DeviseSecurityExtension::Controllers::Helpers
     end
-    
-    ActionDispatch::Callbacks.to_prepare do
-      DeviseSecurityExtension::Patches.apply
-    end
 
+    if defined?(ActiveSupport::Reloader)
+      ActiveSupport::Callbacks.to_prepare do
+        DeviseSecurityExtension::Patches.apply
+      end
+    else
+      ActionDispatch::Callbacks.to_prepare do
+        DeviseSecurityExtension::Patches.apply
+      end
+    end
   end
 end
